8c9706de401980c158b939da0ab349ac39452d1f
joshdsouza8 <joshua@webapp.io>
2021-12-16 14:46:53 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to <a href="https://webapp.io" style={{ color: "white" }}>webapp.io</a>
         </p>
         <a
           className="App-link"
